5530f0528908dd5536a695e3c57d9cf845c0f96e
rana1854 <ranasiddique31@gmail.com>
2024-05-29 21:29:18 +0500

    2nd change

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
     return (
-        <div>Changes made by siddique in Navbar</div>
+        <div>2nd Change made by siddique in Navbar</div>
     )
 }
 

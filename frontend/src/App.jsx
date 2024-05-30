a8637e297c532578c6bc8c4197de9e7f2635def9
Sobia23 <sghani.86@gmail.com>
2024-05-29 23:26:08 -0700

    unknown

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import Home from './pages/home/Home'
+// import Home from './pages/Home'
+// import "./App.css";
+// import { Outlet } from 'react-router-dom';
+
 const App = () => {
   return (
+  <section className='h-screen bg-Hero bg-cover font-[Poppins] md:bg-top bg-center'>
     <div>
       
       < Home/>
       
     </div>
+
+
+
+  </section>
   )
 }
 
-export default App
+export default App;
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import {React} from 'react'
+import { FaSearch } from "react-icons/fa";
 
 const Navbar = () => {
     return (
+<<<<<<< HEAD:frontend/src/components/Navbar.jsx
         <div>Navbar is here</div>
     )
 }
+=======
+        <header>
+            <nav className='bg-white'>
+            <div className='flex item-center font-medium justify-around'>
+                <div>
+                    {/* <img src="" alt="" /> */}
+                    <FaSearch />
+                </div>
+                <ul className="md:flex hidden uppercase items-center gap-8 font-[Poppins]">
+                    <li>
+                        <link to= "/" class="py-7 px-3 inline-block">
+                            Home
+                        </link>
+                        </li>
+                        <Navlinks/>
+                </ul>
+                <div className='md:block hidden'>
+                    <Buttons/>
+                </div>
+>>>>>>> 61862cc (unknown):frontend/src/components/navbar/Navbar.jsx
 
-export default Navbar
+                </div>
+
+            
+            </nav>
+        
+        </header>
+    );
+};
+
+export default Navbar;

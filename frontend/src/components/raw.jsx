905663ac81284614b25b8dea20cb8767b79ddc00
rana1854 <ranasiddique31@gmail.com>
2024-06-02 08:52:04 +0500

    change in component

--- frontend/src/components/raw.jsx
+++ frontend/src/components/raw.jsx
@@ -1,17 +1,24 @@
-// import {
-//   createBrowserRouter,
-//   Routerprovider, 
-// } from "react-router-dom"
+import { createBrowserRouter } from "react-router-dom";
+import App from "../App";
+import Home from "../pages/Home";
+import SingleProduct from "../components/Home/SingleProduct";
 
-// const router = createBrowserRouter([
-// {
-//   path: "/",
-//   element: <App/>,
-//   children = [
-//     {
-//       path: "/",
-//       element: <Home/>
-//   }
-// ]
-// },
-// ]),
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <App/>,
+      children: [
+        {
+            path: "/",
+            element:<Home/>
+        }, 
+        {
+          path: "/shop/:id",
+          element:<SingleProduct/>
+        }
+      ]
+    },
+  ]);
+  
+
+export default router

243c5be9a77b34fc36df775526475b5bb75bc276
rana1854 <ranasiddique31@gmail.com>
2024-06-02 23:13:57 +0500

    some change

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,15 +3,22 @@ import Banner from '../components/home/Banner'
 import Products from '../components/home/Products'
 import BestSellers from '../components/home/BestSellers'
 import Newsletter from '../components/home/Newsletter'
+import Category from "../components/home/Category"
+import Collection from "../components/home/Collection"
+import Singleproduct from "../components/home/Singleproduct"
 
 
 const Home = () => {
   return (
     <div>
-        <Banner/>
-        <Products/>
-        <BestSellers/>
-        <Newsletter/>
+      <Banner />
+      <Products />
+      <BestSellers />
+      <Newsletter />
+      <Category />
+      <Collection />
+      <Singleproduct />
+
     </div>
   )
 }
